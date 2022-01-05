fea8043c20e1b9d1789460d52678c3360b66c546
sivareddy407 <88810375+sivareddy407@users.noreply.github.com>
2022-01-05 21:55:27 +0530

    Update ac2.tf

--- 1/ac2.tf
+++ 1/ac2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-#this is for github demo
+#This is for github demo
 resource "aws_instance" "foo" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
